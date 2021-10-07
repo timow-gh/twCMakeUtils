1adeb0798206e6e65bf780fa2b49c1912141a5f6
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-10-07 22:56:06 +0200

    Fixed Config.cmake generation.

--- modules/CMakeHelperFunctions.cmake
+++ modules/CMakeHelperFunctions.cmake
@@ -35,41 +35,54 @@ function(preamble)
     endif ()
 endfunction()
 
-function(setupExportSetInstall project_name export_set)
+function(setupExportSetInstall proj_config_name export_set_name)
+    include(GNUInstallDirs)
     include(CMakePackageConfigHelpers)
 
-    if (NOT EXISTS "${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in")
+    set(${proj_config_name}_CONFIG_IN_FILE
+            "${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in"
+            CACHE STRING "Path to the ${project_name} Config*.cmake.in file.")
+
+    message(STATUS "${project_name} Config.cmake.in file path: ${${proj_config_name}_CONFIG_IN_FILE}" )
+
+    if (NOT EXISTS "${${proj_config_name}_CONFIG_IN_FILE}")
+        message(STATUS "Absolute Config.cmake.in path: ${${proj_config_name}_CONFIG_IN_FILE}")
         message(FATAL_ERROR "Missing file Config.cmake.in")
         return()
     endif ()
 
-    set(${project_name}_INSTALL_CMAKEDIR
-            "${CMAKE_INSTALL_LIBDIR}/cmake/${project_name}"
-            CACHE STRING "Path to install ${project_name} Config*.cmake files to.")
-    set(${project_name}_MODULE_INSTALL_DIR
-            "${CMAKE_INSTALL_LIBDIR}/cmake"
-            CACHE STRING "Path to install ${project_name}'s .cmake  module files to.")
-
-    install(EXPORT ${export_set}
-            NAMESPACE ${project_name}::
-            FILE ${project_name}Targets.cmake
-            DESTINATION ${${project_name}_INSTALL_CMAKEDIR})
-
     write_basic_package_version_file(
-            ${CMAKE_BINARY_DIR}/${project_name}ConfigVersion.cmake
+            ${CMAKE_BINARY_DIR}/${proj_config_name}ConfigVersion.cmake
             VERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}
             COMPATIBILITY SameMajorVersion)
 
+    # Name of ${proj_config_name}'s targets file.
+    set(projTargetsFileName "${proj_config_name}Targets")
+
+    # The cmake module path for ${proj_config_name}.
+    set(cmakeModulesDir "${CMAKE_INSTALL_LIBDIR}/cmake")
+
+    # Installation path for ${proj_config_name} files.
+    set(cmakeProjDir "${cmakeModulesDir}/${proj_config_name}")
+
+    # Installation path and file name of ${proj_config_name}'s targets file.
+    set(cmakeProjTargetsFilePath "${cmakeProjDir}/${projTargetsFileName}")
+
     configure_package_config_file(
-            ${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in
-            ${CMAKE_BINARY_DIR}/${project_name}Config.cmake
-            INSTALL_DESTINATION ${${project_name}_INSTALL_CMAKEDIR}
-            PATH_VARS ${project_name}_MODULE_INSTALL_DIR
+            ${${proj_config_name}_CONFIG_IN_FILE}
+            ${CMAKE_BINARY_DIR}/${proj_config_name}Config.cmake
+            INSTALL_DESTINATION ${cmakeProjDir}
+            PATH_VARS cmakeModulesDir cmakeProjTargetsFilePath
             NO_SET_AND_CHECK_MACRO
             NO_CHECK_REQUIRED_COMPONENTS_MACRO)
 
+    install(EXPORT ${export_set_name}
+            NAMESPACE ${proj_config_name}::
+            FILE ${projTargetsFileName}.cmake
+            DESTINATION "${cmakeProjDir}/")
+
     install(FILES
-            "${CMAKE_BINARY_DIR}/${project_name}Config.cmake"
-            "${CMAKE_BINARY_DIR}/${project_name}ConfigVersion.cmake"
-            DESTINATION ${${project_name}_INSTALL_CMAKEDIR})
+            "${CMAKE_BINARY_DIR}/${proj_config_name}Config.cmake"
+            "${CMAKE_BINARY_DIR}/${proj_config_name}ConfigVersion.cmake"
+            DESTINATION "${cmakeProjDir}/")
 endfunction()
